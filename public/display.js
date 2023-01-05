076a2d7500c1ecd319501fa8c4f320a92a3e763e
Paul Okoh <pauldokoh@gmail.com>
2023-01-04 20:54:00 -0600

    mvp version 1 ready to present

--- public/display.js
+++ public/display.js
@@ -1,70 +1,54 @@
-const testBtn = document.querySelector('#btn')
-const display  = document.querySelector('#display')
-const jobsBtn = document.querySelector('#findJobs')
+const testBtn = document.querySelector("#btn");
+const display = document.querySelector("#display");
+const jobsBtn = document.querySelector("#findJobs");
 //const {getJob} = require('./update.js')
 
-
-const baseURL = `http://localhost:4004`
+const baseURL = `http://localhost:4004`;
 
 function clearPage() {
-  display.innerHTML = ''
+  display.innerHTML = "";
 }
 
-
 function createCard(job) {
-  const jobCard =
-   `<div class="job-card">
-  <h2>${job['job_title']}</h2>
-  <h3>${job['job_company']}</h3>
-  <h4>${job['job_location']}  ${job['job_zipcode']}</h4>
+  const jobCard = `<div class="job-card">
+  <h2>${job["job_title"]}</h2>
+  <h3>${job["job_company"]}</h3>
+  <h4>${job["job_location"]}  ${job["job_zipcode"]}</h4>
   <button class="btn btn-more">More</button>
   <button class="btn btn-more">Apply</button>
   <button class="btn btn-more"
-  onclick="getUpdatePage(${job['job_id']})">Update</button>
+  onclick="getUpdatePage(${job["job_id"]})">Update</button>
   <button class="btn btn-more"
-  onclick="deleteJob(${job['job_id']})">Delete</button>
-</div>`
-return jobCard
+  onclick="deleteJob(${job["job_id"]})">Delete</button>
+</div>`;
+  return jobCard;
 }
 
-
 function getAllJobs() {
-  clearPage()
-  axios.get(`${baseURL}/jobs`).then((res) => {
-    console.log(res)
-    res.data.forEach(job => {
-      const jobCard = 
-    createCard(job)
+  clearPage();
+  axios
+    .get(`${baseURL}/jobs`)
+    .then((res) => {
+      console.log(res);
+      res.data.forEach((job) => {
+        const jobCard = createCard(job);
 
-    display.innerHTML +=
-    jobCard
-    })  
-    
-  } ).catch(err => console.log(err))
+        display.innerHTML += jobCard;
+      });
+    })
+    .catch((err) => console.log(err));
 }
 
 function deleteJob(id) {
-  axios.delete(`${baseURL}/jobs/${id}`)
-  .then(() => getAllJobs())
-  .catch(err => console.log(err))
+  axios
+    .delete(`${baseURL}/jobs/${id}`)
+    .then(() => getAllJobs())
+    .catch((err) => console.log(err));
 }
 
-getAllJobs()
-
-
-
-
-
-
-
- 
-
-
-
+getAllJobs();
 
 function getUpdatePage(id) {
-    location.href="update.html/"
-    console.log(id)   
+  location.href = "update.html/";
+  console.log(id);
 }
-
-
--- public/index.js
+++ public/index.js
@@ -1,50 +1,43 @@
-const findTitle = document.querySelector('#findTitle')
-const findLocation = document.querySelector('#findLocation')
-const jobsResult  = document.querySelector('#display')
-const searchBtn = document.querySelector('#search')
+const findTitle = document.querySelector("#findTitle");
+const findLocation = document.querySelector("#findLocation");
+const jobsResult = document.querySelector("#display");
+const searchBtn = document.querySelector("#search");
 
-
-const baseURL = `http://localhost:4004`
+const baseURL = `http://localhost:4004`;
 
 function clearResultsPage() {
-  jobsResult.innerHTML = ''
+  jobsResult.innerHTML = "";
 }
 
 function createSearchCard(job) {
-  const searchCard =
-   `<div class="job-card">
-  <h2>${job['job_title']}</h2>
-  <h3>${job['job_company']}</h3>
-  <h4>${job['job_location']}  ${job['job_zipcode']}</h4>
+  const searchCard = `<div class="job-card">
+  <h2>${job["job_title"]}</h2>
+  <h3>${job["job_company"]}</h3>
+  <h4>${job["job_location"]}  ${job["job_zipcode"]}</h4>
   <button class="btn btn-more">More</button>
   <button class="btn btn-more">Apply</button>
   
-</div>`
-return searchCard
+</div>`;
+  return searchCard;
 }
 
 function searchJobs() {
-  clearResultsPage()
-let value = findTitle.value
-  axios.get(`${baseURL}/job/${value}`)
-  .then((res) => {
-    console.log(res)
-    res.data.forEach((value) => {
-      const searchCard = createSearchCard(value)
-      jobsResult.innerHTML += searchCard
+  clearResultsPage();
+  let value = findTitle.value;
+  axios
+    .get(`${baseURL}/job/${value}`)
+    .then((res) => {
+      console.log(res);
+      res.data.forEach((value) => {
+        const searchCard = createSearchCard(value);
+        jobsResult.innerHTML += searchCard;
+      });
     })
-
-  }).catch(err => console.log(err))
-  
+    .catch((err) => console.log(err));
 }
 
-
-searchBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  searchJobs()
-  findTitle.value = ''
-})
-
-
-
-
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchJobs();
+  findTitle.value = "";
+});
